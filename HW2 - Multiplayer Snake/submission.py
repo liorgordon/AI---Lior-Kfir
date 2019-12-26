ec34e28c3268f64bcf39874e656bc9ed9826a366
liorgordon <liorgordon6@gmail.com>
2019-12-26 19:16:45 +0200

    [hw2] added vec impl of SAHC

--- HW2 - Multiplayer Snake/submission.py
+++ HW2 - Multiplayer Snake/submission.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 from environment import Player, GameState, GameAction, get_next_state
 from utils import get_fitness
@@ -118,8 +119,6 @@ class MinimaxAgent(Player):
 
 
 class AlphaBetaAgent(MinimaxAgent):
-    curr_turn = None
-
     class Turn(Enum):
         AGENT_TURN = 'AGENT_TURN'
         OPPONENTS_TURN = 'OPPONENTS_TURN'
@@ -251,6 +250,15 @@ def SAHC_sideways():
     # print("and I got " + str(M))
 
 
+def random_vec(N : int):
+    choices = np.random.choice([0, 1, 2], size=N, replace=True)
+    init = []
+    for item in choices:
+        init.append(GetAction(item))
+    return init
+    pass
+
+
 def SAHC_sideways_vec():
     """
     Implement Steepest Ascent Hill Climbing with Sideways Steps Here.
@@ -265,43 +273,39 @@ def SAHC_sideways_vec():
     :return:
     """
     N = 50
-    init_state = [GameAction.STRAIGHT] * N
+    init_state = random_vec(N)
     sideways = 0
-    limit = N
-    for i in range(N - len(init_state)):
+    limit = N/5
+    for i in range(N):
         best_val = np.NINF
         best_states = None
         for j in range(3):
-            tmp = init_state
+            tmp = init_state.copy()
             if j == 0:
-                tmp += (GameAction.RIGHT,)
+                tmp[i] = GameAction.RIGHT
             elif j == 1:
-                tmp += (GameAction.STRAIGHT,)
+                tmp[i] = GameAction.STRAIGHT
             elif j == 2:
-                tmp += (GameAction.LEFT,)
+                tmp[i] = GameAction.LEFT
             new_val = get_fitness(tmp)
+            if new_val == best_val:
+                best_states.append(tmp)
+
             if new_val > best_val:
                 best_val = new_val
                 best_states = [tmp]
-            elif new_val == best_val:
-                best_states.append(tmp)
         state_fitness = get_fitness(init_state)
         if best_val > state_fitness:
-            new_move = np.random.choice(len(best_states))
-            init_state += (best_states[new_move][-1], )
+            chosen_state = np.random.choice(len(best_states))
+            init_state = best_states[chosen_state]
             sideways = 0
             M=best_val
         elif best_val == state_fitness and sideways <= limit:
-            new_move = np.random.choice(len(best_states))
-            init_state += (best_states[new_move][-1],)
+            chosen_state = np.random.choice(len(best_states))
+            init_state = best_states[chosen_state]
             sideways = sideways + 1
-        else:
-            break
-    if len(init_state) < N:
-        filler = tuple([GameAction.STRAIGHT] * (N-len(init_state)))
-        init_state += filler
-    # print("the best combination I found was {} ", format(init_state))
-    # print("and I got " + str(M))
+    print("the best combination I found was {} ".format(init_state))
+    print("and I got " + str(M))
 
 def GetAction(j):
     switcher = {
@@ -400,11 +404,79 @@ def local_search():
 
 
 class TournamentAgent(Player):
+    class Turn(Enum):
+        AGENT_TURN = 'AGENT_TURN'
+        OPPONENTS_TURN = 'OPPONENTS_TURN'
+
+    class TurnBasedGameState:
+        """
+        This class is a wrapper class for a GameState. It holds the action of our agent as well, so we can model turns
+        in the game (set agent_action=None to indicate that our agent has yet to pick an action).
+        """
+
+        def __init__(self, game_state: GameState, agent_action: GameAction):
+            self.game_state = game_state
+            self.agent_action = agent_action
+
+        @property
+        def turn(self):
+            return MinimaxAgent.Turn.AGENT_TURN if self.agent_action is None else MinimaxAgent.Turn.OPPONENTS_TURN
+
+    def abminimax(self, state: TurnBasedGameState, D: int, alpha, beta):
+        if D == 0 or not state.game_state.snakes[self.player_index].alive or state.game_state.is_terminal_state:
+            return heuristic(state.game_state, self.player_index)
+        best_value = -np.inf
+        worst_value = np.inf
+        if state.agent_action is None:
+            for our_action in state.game_state.get_possible_actions(player_index=self.player_index):
+                h_value = self.abminimax(self.TurnBasedGameState(state.game_state, our_action), D, alpha, beta)
+                if h_value > best_value:
+                    best_value = h_value
+                    alpha = max(alpha, best_value)
+                if best_value >= beta:
+                    # print("cut beta in depth: {}, beta is: {}, alpha is: {}".format(D, beta, alpha))
+                    return np.inf
+            # if not state.game_state.snakes[self.player_index].alive:
+            # print("i entered agent with a dead snake, returning {}", format(best_value))
+            return best_value
+        else:
+            for opponents_actions in state.game_state.get_possible_actions_dicts_given_action(state.agent_action,
+                                                                                              player_index=self.player_index):
+                opponents_actions[self.player_index] = state.agent_action
+                next_state = get_next_state(state.game_state, opponents_actions)
+                # print("entered None ")
+                h_value = self.abminimax(self.TurnBasedGameState(next_state, None), D - 1, alpha, beta)
+                if h_value < worst_value:
+                    worst_value = h_value
+                    beta = min(worst_value, beta)
+                if worst_value <= alpha:
+                    # print("cut alpha in depth: {}, beta is: {}, alpha is: {}".format(D, beta, alpha))
+                    return -np.inf
+            return worst_value
 
     def get_action(self, state: GameState) -> GameAction:
-        pass
+        D_arr = [2, 3, 4]
+
+        best_value = -np.inf
+        best_actions = []
+        self.curr_turn.curr_turn = MinimaxAgent.Turn.OPPONENTS_TURN
+        i=0
+        for our_action in state.get_possible_actions(player_index=self.player_index):
+            t = time.time()
+            h_value = self.abminimax(self.TurnBasedGameState(state, our_action), D_arr[i], -np.inf, np.inf)
+            elapsed = time.time() - t
+            if elapsed < 15:
+                i += 1
+            # elif elapsed
+            if h_value > best_value:
+                best_value = h_value
+                best_actions = [our_action]
+            elif h_value == best_value:
+                best_actions.append(our_action)
+        return np.random.choice(best_actions)
 
 
 if __name__ == '__main__':
     # SAHC_sideways()
-    local_search()
+    SAHC_sideways_vec()
+    #local_search()
